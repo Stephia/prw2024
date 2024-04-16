b6004fd92194b2e89eac471c8bb3c2ea734dd71b
Stephanie Forcassin Hatida <103062490+Stephia@users.noreply.github.com>
2024-04-16 02:24:37 -0300

    Create 1035.php

---
+++ Lista2/1035.php
@@ -0,0 +1,15 @@
+<?php
+
+list($A, $B, $C, $D) = explode(' ', readline());
+
+if($B > $C && $D > $A && ($C + $D > $A + $B) && $C + $D > 0 && $A % 2 == 0)
+{
+    echo "Valores aceitos\n";
+}
+  
+else
+{
+    echo "Valores nao aceitos\n";
+}
+
+?>
