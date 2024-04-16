3573109d6fb6d22b43119a5fb7e769e42d9b7992
Stephanie Forcassin Hatida <103062490+Stephia@users.noreply.github.com>
2024-04-16 02:23:08 -0300

    Update 1016.php

--- Lista1/1016.php
+++ Lista1/1016.php
@@ -1,1 +1,12 @@
+<?php
 
+// Lendo a distância em quilômetros
+$distancia = intval(readline());
+
+// Calculando o tempo necessário em minutos
+$tempo_necessario = $distancia * 2;
+
+// Imprimindo o tempo necessário seguido da mensagem "minutos"
+echo $tempo_necessario . " minutos\n";
+
+?>
